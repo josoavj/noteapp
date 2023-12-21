86d7273026829b015b7538c6d824742d404ba75a
Josoa Vonjiniaina <76913187+Josoa886@users.noreply.github.com>
2023-12-21 17:54:02 +0300

    Mise a jour de la page note

--- lib/models/notelist.dart
+++ lib/models/notelist.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Note extends StatelessWidget{
   Note({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(itemBuilder: itemBuilder, separatorBuilder: separatorBuilder, itemCount: itemCount);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Note List",
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      extendBodyBehindAppBar: false,
+      body: ListView.separated(itemBuilder: itemBuilder, separatorBuilder: separatorBuilder, itemCount: itemCount),
+    );
   }
 }
